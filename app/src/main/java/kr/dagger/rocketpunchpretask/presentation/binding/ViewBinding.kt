ebe6e86232fd47eebe8f71dd8d38f590d1a6477a
3dagger <dlqtn36@gmail.com>
2022-11-03 11:07:38 +0900

    add: BindingAdapter

---
+++ app/src/main/java/kr/dagger/rocketpunchpretask/presentation/binding/ViewBinding.kt
@@ -0,0 +1,42 @@
+package kr.dagger.rocketpunchpretask.presentation.binding
+
+import android.net.Uri
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import kr.dagger.rocketpunchpretask.R
+
+object ViewBinding {
+
+	@JvmStatic
+	@BindingAdapter("bindImage")
+	fun ImageView.bindImage(url: Uri?) {
+		val options = RequestOptions()
+			.placeholder(R.drawable.ic_rocket_punch_logo_background)
+			.error(R.drawable.ic_rocket_punch_logo_background)
+
+		url?.let {
+			Glide.with(this.context).load(it).apply(options).into(this)
+		} ?: run {
+			Glide.with(this.context).load(R.drawable.rocket_punch_login).into(this)
+		}
+	}
+
+	@JvmStatic
+	@BindingAdapter("bindStringImage")
+	fun ImageView.bindStringImage(url: String?) {
+		val options = RequestOptions()
+			.placeholder(R.drawable.ic_rocket_punch_logo_background)
+			.error(R.drawable.ic_rocket_punch_logo_background)
+
+		url?.let {
+			Glide.with(this.context).load(it).apply(options).into(this)
+		} ?: run {
+			Glide.with(this.context).load(R.drawable.rocket_punch_login).into(this)
+		}
+	}
+
+}
