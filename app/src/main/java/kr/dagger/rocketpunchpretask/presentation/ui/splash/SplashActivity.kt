a3d01c111466d0c35072f2d9e9e7eb42040ea7f1
3dagger <dlqtn36@gmail.com>
2022-11-03 10:58:51 +0900

    feat: Logged User Check Function

--- app/src/main/java/kr/dagger/rocketpunchpretask/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/kr/dagger/rocketpunchpretask/presentation/ui/splash/SplashActivity.kt
@@ -5,25 +5,42 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.window.SplashScreen
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.dagger.rocketpunchpretask.R
 import kr.dagger.rocketpunchpretask.base.BaseActivity
 import kr.dagger.rocketpunchpretask.databinding.ActivitySplashBinding
+import kr.dagger.rocketpunchpretask.presentation.extension.openActivity
+import kr.dagger.rocketpunchpretask.presentation.ui.MainActivity
 import kr.dagger.rocketpunchpretask.presentation.ui.login.LoginActivity
 
 @SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
+	private val viewModel: SplashViewModel by viewModels()
+
 	override fun initView(savedInstanceState: Bundle?) {
-		Handler(Looper.myLooper()!!).postDelayed({
-			startActivity(Intent(this, LoginActivity::class.java))
-		}, 1500)
+	}
+
+	override fun subscribeObservers() {
+		viewModel.isLoggedUser.observe(this@SplashActivity) {
+			when (it) {
+				true -> moveMain()
+				false -> moveLogin()
+			}
+		}
+	}
+
+	private fun moveLogin() {
+		openActivity(LoginActivity::class.java)
 		finish()
 	}
-	override fun subscribeObservers() {
 
+	private fun moveMain() {
+		openActivity(MainActivity::class.java)
+		finish()
 	}
-
 }
--- app/src/main/java/kr/dagger/rocketpunchpretask/presentation/ui/splash/SplashViewModel.kt
+++ app/src/main/java/kr/dagger/rocketpunchpretask/presentation/ui/splash/SplashViewModel.kt
@@ -1,8 +1,35 @@
 package kr.dagger.rocketpunchpretask.presentation.ui.splash
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kr.dagger.domain.usecase.auth.GetIsLoggedUserUseCase
+import kr.dagger.domain.usecase.auth.GetUserLoginUseCase
+import javax.inject.Inject
 
-//@HiltViewModel
-class SplashViewModel : ViewModel() {
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+	private val getUserLoginUseCase: GetUserLoginUseCase,
+	private val getIsLoggedUserUseCase: GetIsLoggedUserUseCase
+)  : ViewModel() {
+
+//	private val _isLoginUser = MutableLiveData<Boolean>()
+//	val isLoginUser : LiveData<Boolean> = _isLoginUser
+
+	private val _isLoggedUser = MutableLiveData<Boolean>()
+	val isLoggedUser: LiveData<Boolean> = _isLoggedUser
+
+	init {
+		viewModelScope.launch {
+			_isLoggedUser.value = getIsLoggedUserUseCase.invoke()
+		}
+
+		viewModelScope.launch {
+			getUserLoginUseCase.invoke().collectLatest {
+//				_isLoginUser.value = it
+			}
+		}
+	}
+
 }
