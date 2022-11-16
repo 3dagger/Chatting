bf219aa1f48a22e786376a6f3344f15cba39c620
3dagger <dlqtn36@gmail.com>
2022-11-16 09:09:13 +0900

    add: SignUp

---
+++ app/src/main/java/kr/dagger/chat/presentation/ui/sign/siginup/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package kr.dagger.chat.presentation.ui.sign.siginup
+
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import kr.dagger.chat.R
+import kr.dagger.chat.base.BaseActivity
+import kr.dagger.chat.databinding.ActivitySignUpBinding
+
+@AndroidEntryPoint
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+	override fun initView(savedInstanceState: Bundle?) {
+
+	}
+}
