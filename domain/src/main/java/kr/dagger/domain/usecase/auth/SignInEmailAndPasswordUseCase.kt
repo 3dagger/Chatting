cd8036925ae8d017e7bff47b7dae9f6cf159c967
3dagger <dlqtn36@gmail.com>
2022-12-05 02:21:57 +0900

    refactor: Return type change

--- domain/src/main/java/kr/dagger/domain/usecase/auth/SignInEmailAndPasswordUseCase.kt
+++ domain/src/main/java/kr/dagger/domain/usecase/auth/SignInEmailAndPasswordUseCase.kt
@@ -2,12 +2,13 @@ package kr.dagger.domain.usecase.auth
 
 import kotlinx.coroutines.flow.Flow
 import kr.dagger.domain.model.Response
+import kr.dagger.domain.model.UserInfo
 import kr.dagger.domain.repository.AuthRepository
 
 class SignInEmailAndPasswordUseCase(
 	private val repository: AuthRepository
 ) {
-	suspend operator fun invoke(email: String, password: String) : Flow<Response<Unit>> {
+	suspend operator fun invoke(email: String, password: String) : Flow<Response<UserInfo>> {
 		return repository.signInEmailAndPassword(email, password)
 	}
 }
