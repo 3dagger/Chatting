194355a554e5bb3dbf97e95dff6d40b65a53e00a
3dagger <dlqtn36@gmail.com>
2022-12-06 16:25:25 +0900

    add: Constants add

--- app/src/main/java/kr/dagger/chat/presentation/ui/Constants.kt
+++ app/src/main/java/kr/dagger/chat/presentation/ui/Constants.kt
@@ -1,6 +1,8 @@
 package kr.dagger.chat.presentation.ui
 
 object Constants {
+	const val INTENT_SIGN_UP_RESULT = "INTENT_SIGN_UP_RESULT"
+
 	const val RESULT_CODE_GOOGLE_SIGN_IN = 999
 	const val RESULT_CODE_SIGN_UP = 998
 }
